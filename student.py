97359c4a35913d7c856728a6fc4feabbe51400a3
Shrinivas <shrinivas4927@gmail.com>
2025-11-28 23:26:24 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,13 +1,12 @@
 import sys
 
-
-scores = [10, 20, 30, 40, 50]
-
+default_scores = [10, 20, 30, 40, 50]
 
 if len(sys.argv) <= 1:
+    scores = default_scores
     print("No scores provided. Using default values:", scores)
 else:
-    scores = [int(num) for num in sys.argv[1:]]
+    scores = [float(num) for num in sys.argv[1:]]
 
 total = sum(scores)
 average = total / len(scores)
